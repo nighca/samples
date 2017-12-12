60e1c6d8978ece16698c3537f1f56a4e1fb41e4a
nighca <nighca@live.cn>
2017-12-12 17:37:27 +0800

    add typescript-react

---
+++ typescript-react/src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import Svg from './Svg'
+
+const logo = require('./logo.svg')
+
+class App extends React.Component<{}, void> {
+  render() {
+    return (
+      <div className="App">
+        <Svg src={logo} />
+        Hello!
+      </div>
+    );
+  }
+}
+
+export default App
---
+++ typescript-react/src/Svg.tsx
@@ -0,0 +1,24 @@
+/*
+ * @file component Svg
+ * @author nighca <nighca@live.cn>
+ */
+
+import * as React from 'react'
+
+export interface ISvgProps extends React.SVGAttributes<SVGElement> {
+  src: string
+}
+
+export default function Svg(props: ISvgProps) {
+  const { src, ...others } = props
+  debugger
+
+  return (
+    <svg {...others}>
+      <use
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        xlinkHref={src}
+      ></use>
+    </svg>
+  )
+}
