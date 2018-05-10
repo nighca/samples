c6e7220f56b34431dd5bcfb6c399b2823d0c659a
nighca <nighca@live.cn>
2018-05-10 22:22:17 +0800

    test files

---
+++ hello-world/src/index.spec.js
@@ -0,0 +1,7 @@
+import './index'
+
+describe('index', () => {
+  it('should work correctly', () => {
+    expect(document.body.innerHTML).toBe('hello world!')
+  })
+})
---
+++ hello-world/src/names.spec.js
@@ -0,0 +1,7 @@
+import { world } from './names'
+
+describe('names', () => {
+  it('should be correct', () => {
+    expect(world).toBe('world')
+  })
+})
