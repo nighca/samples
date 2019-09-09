2e04c1117a232e89ae6c00051a68cfeb2413d370
nighca <nighca@live.cn>
2019-09-09 15:17:22 +0800

    avoid npx for node 6.x

--- transform-deps/fec-test.sh
+++ transform-deps/fec-test.sh
@@ -1,6 +1,7 @@
 echo 'Building sample/transform-deps!'
 
+npm i
 fec-builder clean && fec-builder generate -e production
 
 # 检查 node_modules 中的 ES6 内容是否被正确转换
-npx es-check es5 dist/static/*.js
+./node_modules/.bin/es-check es5 ./dist/static/*.js
