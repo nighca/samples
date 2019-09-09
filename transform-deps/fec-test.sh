f9af98397a6109dc38ffadeb737d92f68046b47c
nighca <nighca@live.cn>
2019-09-09 15:08:44 +0800

    use es-check to check js transform

--- transform-deps/fec-test.sh
+++ transform-deps/fec-test.sh
@@ -2,11 +2,5 @@ echo 'Building sample/transform-deps!'
 
 fec-builder clean && fec-builder generate -e production
 
-# 检查 node_modules 中的箭头函数是否被正确转换
-if [[ "$(cat dist/static/*.js | grep "=>")"x != ""x ]]; then
-  echo 'Error.'
-  exit 1
-else
-  echo 'Success.'
-  exit 0
-fi
+# 检查 node_modules 中的 ES6 内容是否被正确转换
+npx es-check es5 dist/static/*.js
