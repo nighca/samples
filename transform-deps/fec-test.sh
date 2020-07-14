196b4b86e09439af07d58996964e18d0606bd73c
nighca <nighca@live.cn>
2020-07-14 12:15:59 +0800

    debug test script for transform-deps within node 12.x

--- transform-deps/fec-test.sh
+++ transform-deps/fec-test.sh
@@ -1,6 +1,10 @@
 echo 'Building sample/transform-deps!'
 
+# Node 12.x 对应的 npm 会把 node_modules 目录下的内容（如这里的 foo/）干掉
+mv node_modules/foo foo
 npm i
+mv foo node_modules/foo
+
 fec-builder clean && fec-builder generate -e production
 
 # 检查 node_modules 中的 ES6 内容是否被正确转换
